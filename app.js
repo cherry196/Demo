8754d875b149aaaf89a43051fb8e32bebc20e77e
cherry196 <35716387+cherry196@users.noreply.github.com>
2018-02-11 20:56:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const path = require('path');
-//Init App
+const http = require('http');
 const app = express();
-
+const server = require('http').Server(app);
+var bodyparser = require('body-parser');
+var urlParser = bodyparser.urlencoded({extended:false});
 app.use('/resources',express.static(path.join(__dirname,'resources')));
 app.set('views',__dirname+'/views');
 app.engine('html', require('ejs').renderFile);
-
 app.set('view engine','html')
-//Get request matching '/'
+//Get request matching
 app.get('/' ,function(req,res){
-  res.render('home');//Response Hello World
+  res.render('home');
 });
-
-const port = process.env.PORT || 3000
+app.post('/' ,urlParser, function(req,res){
+  console.log(req.body);
+  res.render('home');
+});
+const port = process.env.PORT || 8080;
 app.listen(port, function() {
 console.log('Express server running on '+port);
-} )
+})
